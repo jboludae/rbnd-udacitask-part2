b7c9dafbd55e1f1a744e953c71e3405fbe663aba
jboludae <jboludae@gmail.com>
2016-03-12 12:20:22 +0100

    Refactored the format_date method

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,5 +1,12 @@
+
 module Listable
   def format_description(description)
     "#{description}".ljust(25)
+  end
+  def format_date(*dates_list)
+    dates = dates_list[0].strftime("%D") if dates_list[0]
+    dates << " -- " + dates_list[1].strftime("%D") if dates_list[1]
+    dates = "N/A" if !dates
+    return dates
   end
 end
