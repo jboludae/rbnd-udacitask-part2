1f61df38ddb725c77344102700eec2e89c09590c
jboludae <jboludae@gmail.com>
2016-03-12 16:43:31 +0100

    Colorize module is now working

--- lib/listable.rb
+++ lib/listable.rb
@@ -10,9 +10,9 @@ module Listable
     return dates
   end
   def format_priority(priority)
-    value = " ⇧" if priority == "high"
-    value = " ⇨" if priority == "medium"
-    value = " ⇩" if priority == "low"
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:magenta) if priority == "medium"
+    value = " ⇩".colorize(:yellow) if priority == "low"
     value = "" if !priority
     return value
   end
